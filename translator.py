895603926b1bd37d45da692533bbe8dc17ded951
elizarpif <liza.pif@mail.ru>
2022-03-31 14:29:28 +0300

    try to add another languages

--- translator.py
+++ translator.py
@@ -1,22 +1,57 @@
 from googletrans import Translator
 from gtts import gTTS
 from telegram import Update
+import config
 
 translator = Translator()
 
-# 0 = ru-tr, 1 = tr-ru
-Current_mode = 0
-Multi = False
+
+class Language:
+    def __init__(self, current_foreign='tr', current_base='ru'):
+        self.current_foreign_lang = current_foreign
+        self.current_base_lang = current_base
+        self.multi = False
+        self.switcher_long_short = {
+            'Turkish': 'tr',
+            'English': 'en',
+            'Serbian': 'sr',
+        }
+        self.switcher_short_long = {
+            'tr': 'Turkish',
+            'en': 'English',
+            'sr': 'Serbian',
+        }
+
+    def set_foreign(self, lang='tr'):
+        self.current_foreign_lang = lang
+
+    def set_base(self, lang='ru'):
+        self.current_base_lang = lang
+
+    def get_foreign_string(self):
+        return self.switcher_short_long.get(self.current_foreign_lang)
+
+    def get_foreign(self):
+        return self.current_foreign_lang
+
+    def get_base(self):
+        return self.current_base_lang
+
+    def enable_multi_mode(self):
+        self.multi = True
+
+    def disable_multi_mode(self):
+        self.multi = False
 
 
-def set_ru():
+def set_base():
     global Current_mode
     Current_mode = 0
     global Multi
     Multi = False
 
 
-def set_tr():
+def set_foreign():
     global Current_mode
     Current_mode = 1
     global Multi
@@ -28,68 +63,50 @@ def set_multi():
     Multi = True
 
 
-def switch_dest():
-    if Current_mode == 0:
-        return 'tr'
-
-    return 'ru'
-
-
-def switch_src():
-    if Current_mode == 0:
-        return 'ru'
-
-    return 'tr'
-
-
-filename = 'voice2.ogg'
-
-
-def voice(text):
-    tts = gTTS(text, lang=switch_dest())
-    tts.save(filename)
+def voice(text, lang: Language):
+    tts = gTTS(text, lang=lang.get_foreign())
+    tts.save(config.FILENAME)
 
 
 def voice_lang(text, lang='tr'):
     tts = gTTS(text, lang=lang)
-    tts.save(filename)
-
-
-ru_lang = 'ru'
-en_lang = 'en'
+    tts.save(config.FILENAME)
 
 
 # multi_translate translates in Multi mode, when we dont know what language
-def multi_translate(update: Update):
+def multi_translate(update: Update, lang: Language):
     """
     переводит в обе стороны
     :param update:
     """
     text = update.message.text
 
-    rutu = translator.translate(text, dest='tr').text
-    turu = translator.translate(text, dest='ru').text
+    tr = lang.get_foreign()
+    ru = lang.get_base()
+
+    rutu = translator.translate(text, dest=tr).text
+    turu = translator.translate(text, dest=ru).text
 
     # делаем озвучку и отправляем ее
-    voice_lang(rutu, 'tr')
-    with open(filename, 'rb') as f:
+    voice_lang(rutu, tr)
+    with open(config.FILENAME, 'rb') as f:
         # отправляем
         update.message.reply_voice(f, caption=rutu)
 
     # делаем озвучку и отправляем ее
-    voice_lang(turu, 'ru')
-    with open(filename, 'rb') as f:
+    voice_lang(turu, ru)
+    with open(config.FILENAME, 'rb') as f:
         # отправляем
         update.message.reply_voice(f, caption=turu)
 
 
 # translate in usual mode (Ru->Tr or Tr->Ru)
-def translate(update: Update):
-    res = translator.translate(update.message.text, switch_dest(), switch_src()).text
+def translate(update: Update, lang: Language):
+    res = translator.translate(update.message.text, dest=lang.get_foreign(), src=lang.get_base()).text
 
     # делаем озвучку и отправляем ее
-    voice(res)
+    voice(res, lang=lang)
 
-    with open(filename, 'rb') as f:
+    with open(config.FILENAME, 'rb') as f:
         # отправляем
         update.message.reply_voice(f, caption=res)
